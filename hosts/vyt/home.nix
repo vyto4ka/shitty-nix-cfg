ba1d76b94e3dc1c3930411e8171416556f060e50
Татауров Степан <s.tataurov@okbsapr.com>
2025-04-15 16:21:10 +0300

    lazynvim try

--- hosts/vyt/home.nix
+++ hosts/vyt/home.nix
@@ -30,11 +30,6 @@
   home.file.".config/kitty/kitty.conf".source = ./kitty/kitty.conf;
   home.file.".config/kitty/current-theme.conf".source = ./kitty/current-theme.conf;
 
-  home.file.".config/nvim" = {
-    source = ./nvim;
-    recursive = true;
-  };
-
   programs.neovim = {
     enable = true;
     extraPackages = with pkgs; [ git ];
