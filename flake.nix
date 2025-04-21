07fa17adcecdbd8b3d0920f37f2971eb25c648f3
Стёпа Та <64610654+vyto4ka@users.noreply.github.com>
2025-04-22 00:11:45 +0300

    ОТКАТ БЛЯтт

--- flake.nix
+++ flake.nix
@@ -1,50 +1,22 @@
 {
-  description = "NixOS 24.11 + Home Manager для vyt";
-
+  description = "NixOS 24.11 config with Home Manager for multiple machines";
   inputs = {
-    # основная nixpkgs — именно ветка 24.11
-    nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixos-24.11";
-    };
-
-    # home-manager той же версии, что и nixpkgs
-    home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    home-manager.url = "github:nix-community/home-manager";
   };
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
-  let
-    system = "x86_64-linux";
-
-    # импортируем nixpkgs как полноценный package-set,
-    # сразу отключая модуль labwc
-    pkgs = import nixpkgs {
-      inherit system;
-      config = {
-        disabledModules = [ "services/window-managers/labwc.nix" ];
-      };
-    };
-  in {
-    # NixOS‑конфигурация “vyt”
     nixosConfigurations = {
-      vyt = pkgs.lib.nixosSystem {
-        inherit system;
-
-        modules = [
-          ./hosts/vyt/configuration.nix
-        ];
-
-        # чтобы внутри configuration.nix был доступ к inputs
+      "vyt" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./hosts/vyt/configuration.nix ];
         specialArgs = { inherit inputs; };
       };
     };
 
-    # Home Manager для пользователя
     homeConfigurations = {
       "vyto4ka@vyt" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
         modules = [ ./hosts/vyt/home.nix ];
         extraSpecialArgs = { inherit inputs; };
       };
