8fb4dd747b61865cc05db3a225c1ee54b48950b2
Стёпа Та <64610654+vyto4ka@users.noreply.github.com>
2025-04-22 00:12:40 +0300

    Откат блятт

--- hosts/vyt/configuration.nix
+++ hosts/vyt/configuration.nix
@@ -1,5 +1,14 @@
 { config, pkgs, inputs, ... }:
+
 {
+
+{
+  nixpkgs.config = {
+    disabledModules = [
+      "services/window-managers/labwc.nix"
+    ];
+  };
+
   imports = 
   [ 
     ./hardware-configuration.nix
