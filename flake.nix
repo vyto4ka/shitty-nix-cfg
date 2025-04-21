40becf2842024cb463a08c83ca8c431e87b9b87a
Стёпа Та <64610654+vyto4ka@users.noreply.github.com>
2025-04-22 00:49:53 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,8 +1,9 @@
 {
   description = "NixOS 24.11 config with Home Manager for multiple machines";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
 
