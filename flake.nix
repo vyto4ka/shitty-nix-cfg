a5c262b04c90dc369a277aa3c5c5804f9d424eab
Татауров Степан <s.tataurov@okbsapr.com>
2025-04-22 17:35:56 +0300

    add unfree

--- flake.nix
+++ flake.nix
@@ -1,18 +1,24 @@
 {
   description = "NixOS 24.11 config with Home Manager for multiple machines";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
+
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
 
     nixosConfigurations = {
       "vyt" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
+
+        # Ваши модули
         modules = [ ./hosts/vyt/configuration.nix ];
 
-        nixpkgs = {
+        # Собираем pkgs с allowUnfree
+        pkgs = import nixpkgs {
+          inherit system;
           config = {
             allowUnfree = true;
           };
