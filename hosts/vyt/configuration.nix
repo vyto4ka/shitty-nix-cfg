4af176a0f2bd6bcbe4ebf4c4dc90b1a142cc9256
Татауров Степан <s.tataurov@okbsapr.com>
2025-04-22 17:01:32 +0300

    add locales

--- hosts/vyt/configuration.nix
+++ hosts/vyt/configuration.nix
@@ -20,6 +20,24 @@
   i18n.defaultLocale = "ru_RU.UTF-8";
   time.timeZone = "Europe/Moscow";
 
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "ru_RU.UTF-8";
+    LC_IDENTIFICATION = "ru_RU.UTF-8";
+    LC_MEASUREMENT = "ru_RU.UTF-8";
+    LC_MONETARY = "ru_RU.UTF-8";
+    LC_NAME = "ru_RU.UTF-8";
+    LC_NUMERIC = "ru_RU.UTF-8";
+    LC_PAPER = "ru_RU.UTF-8";
+    LC_TELEPHONE = "ru_RU.UTF-8";
+    LC_TIME = "ru_RU.UTF-8";
+  };
+
+  services.xserver.xkb = {
+    layout = "us,ru";
+    variant = ",";
+    options = "grp:alt_shift_toggle";
+  };
+  
   services.xserver.enable = true;
 
   services.displayManager.sddm.enable = true;
