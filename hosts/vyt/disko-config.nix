7ad1d7e540ed98c16d9824a3ed0f9ef27b4b942f
Татауров Степан <s.tataurov@okbsapr.com>
2025-04-28 16:27:58 +0300

    try fix disko

--- hosts/vyt/disko-config.nix
+++ hosts/vyt/disko-config.nix
@@ -1,32 +1,35 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, … }:
+
 {
-  disko.devices.sda = {
-    device = "/dev/sda";
-    content = {
-      type    = "table";
-      format  = "gpt";
-      partitions = [
-        {
-          name      = "ESP";
-          start     = "1MiB";
-          end       = "512MiB";
-          content = {
-            type       = "filesystem";
-            format     = "vfat";
-            mountpoint = "/boot";
-          };
-        }
-        {
-          name      = "root";
-          start     = "512MiB";
-          end       = "100%";
-          content = {
-            type       = "filesystem";
-            format     = "ext4";
-            mountpoint = "/";
-          };
-        }
-      ];
+  disko.devices = {
+    sda = {
+      device = "/dev/sda";
+      content = {
+        type       = "table";
+        format     = "gpt";
+        partitions = [
+          {
+            name      = "ESP";
+            start     = "1MiB";
+            end       = "512MiB";
+            content = {
+              type       = "filesystem";
+              format     = "vfat";
+              mountpoint = "/boot";
+            };
+          }
+          {
+            name      = "root";
+            start     = "512MiB";
+            end       = "100%";
+            content = {
+              type       = "filesystem";
+              format     = "ext4";
+              mountpoint = "/";
+            };
+          }
+        ];
+      };
     };
   };
 }
