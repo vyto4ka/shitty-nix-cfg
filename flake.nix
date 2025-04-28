425dd3ca348608304ee76196459062cf99385307
Татауров Степан <s.tataurov@okbsapr.com>
2025-04-28 16:24:59 +0300

    try fix disko

--- flake.nix
+++ flake.nix
@@ -9,34 +9,25 @@
     disko.inputs.nixpkgs.follows        = "nixpkgs";
   };
 
-outputs = inputs@{ … }: let
+outputs = inputs@{ self, nixpkgs, disko, ... }: let
   system      = "x86_64-linux";
-  nixpkgs      = inputs.nixpkgs;
-  disko        = inputs.disko;
-  # Вместо деструктуризации с дефисом — просто берём из inputs
-  homeManager  = inputs."home-manager";
-
-  # Ваши unfree-пакеты
-  pkgsUnfree = import nixpkgs {
+  homeManager = inputs."home-manager";
+  pkgsUnfree  = import nixpkgs {
     inherit system;
     config = { allowUnfree = true; };
   };
 in {
-  # NixOS-конфигурация
-  nixosConfigurations = {
-    vyt = nixpkgs.lib.nixosSystem {
-      inherit system;
-      modules = [
-        disko.nixosModules.disko
-        ./hosts/vyt/disko-config.nix
-        ./hosts/vyt/configuration.nix
-      ];
-      pkgs        = pkgsUnfree;
-      specialArgs = { inherit inputs; };
-    };
+  nixosConfigurations.vyt = nixpkgs.lib.nixosSystem {
+    inherit system;
+    modules = [
+      disko.nixosModules.disko
+      ./hosts/vyt/disko-config.nix
+      ./hosts/vyt/configuration.nix
+    ];
+    pkgs        = pkgsUnfree;
+    specialArgs = { inherit inputs; };
   };
 
-  # Home-Manager-конфигурация
   homeConfigurations = {
     "vyto4ka@vyt" = homeManager.lib.homeManagerConfiguration {
       pkgs             = nixpkgs.legacyPackages.${system};
@@ -46,3 +37,4 @@ in {
   };
 };
 }
+
