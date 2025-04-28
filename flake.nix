c24f44f7327fa3185e3e3a1ae647e1c32818f24c
Татауров Степан <s.tataurov@okbsapr.com>
2025-04-28 16:18:59 +0300

    try fix disko

--- flake.nix
+++ flake.nix
@@ -9,34 +9,31 @@
     disko.inputs.nixpkgs.follows        = "nixpkgs";
   };
 
- outputs = inputs@{ self,
-                   nixpkgs,
-                   disko,
-                   "home-manager" = homeManager,
-                   ... }:
-    let
-      system    = "x86_64-linux";
-      pkgsUnfree = import nixpkgs { inherit system; config = { allowUnfree = true; }; };
-    in {
-      nixosConfigurations = {
-        "vyt" = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            disko.nixosModules.disko
-            ./hosts/vyt/disko-config.nix
-            ./hosts/vyt/configuration.nix
-          ];
-          pkgs         = pkgsUnfree;
-          specialArgs  = { inherit inputs; };
-        };
-      };
-
-    homeConfigurations = {
-      "vyto4ka@vyt" = homeManager.lib.homeManagerConfiguration {
-        pkgs             = nixpkgs.legacyPackages.${system};
-        modules          = [ ./hosts/vyt/home.nix ];
-        extraSpecialArgs = { inherit inputs; };
-      };
+ outputs = inputs@{ nixpkgs, disko, ... }: let
+  system     = "x86_64-linux";
+  pkgsUnfree = import nixpkgs { inherit system; config = { allowUnfree = true; }; };
+  # Алиас для home-manager берём из inputs
+  homeManager = inputs."home-manager";
+in {
+  nixosConfigurations = {
+    "vyt" = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        disko.nixosModules.disko
+        ./hosts/vyt/disko-config.nix
+        ./hosts/vyt/configuration.nix
+      ];
+      pkgs        = pkgsUnfree;
+      specialArgs = { inherit inputs; };
     };
   };
+
+  homeConfigurations = {
+    "vyto4ka@vyt" = homeManager.lib.homeManagerConfiguration {
+      pkgs             = nixpkgs.legacyPackages.${system};
+      modules          = [ ./hosts/vyt/home.nix ];
+      extraSpecialArgs = { inherit inputs; };
+    };
+  };
+};
 }
