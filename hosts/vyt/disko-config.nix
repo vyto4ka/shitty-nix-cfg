41f44a2e3e252b4c1da5ea8de3414df4179f1233
Татауров Степан <s.tataurov@okbsapr.com>
2025-04-28 16:37:13 +0300

    try fix disko

--- hosts/vyt/disko-config.nix
+++ hosts/vyt/disko-config.nix
@@ -6,27 +6,32 @@
       sda = {
         device = "/dev/sda";
         content = {
-          type    = "table";
-          format  = "gpt";
-           partitions = {
-             ESP = {
-               size = "512MiB";
-               content = {
-                 type       = "filesystem";
-                 format     = "vfat";
-                 mountpoint = "/boot";
-                 mountOptions = [ "umask=0077" ];
-               };
-             };
-             root = {
-               size = "100%";
-               content = {
-                 type       = "filesystem";
-                 format     = "ext4";
-                 mountpoint = "/";
-               };
-             };
-           };
+          type   = "table";
+          format = "gpt";
+          partitions = [
+            {
+              name      = "ESP";
+              start     = "1MiB";
+              end       = "512MiB";
+              bootable  = true;         # нужно для BIOS-MBR/BIOS-GRUB
+              content = {
+                type        = "filesystem";
+                format      = "vfat";
+                mountpoint  = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            }
+            {
+              name      = "root";
+              start     = "512MiB";
+              end       = "100%";
+              content = {
+                type       = "filesystem";
+                format     = "ext4";
+                mountpoint = "/";
+              };
+            }
+          ];
         };
       };
     };
